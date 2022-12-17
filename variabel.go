87ac4f059e8616fa478079bbbc38886a154238f1
Ilham Budiawan Sitorus <budiawanilham04@gmail.com>
2022-12-17 20:14:55 +0700

    Variabel dan Tipe Data

---
+++ variabel.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main(){
+	var name string = "Ilham Budiawan"
+	var age int = 20
+	fmt.Println(name)
+	fmt.Println(age)
+
+	var nameTwo string
+	var ageTwo int
+	nameTwo = "Bela Nabila"
+	ageTwo = 15
+	fmt.Println(nameTwo)
+	fmt.Println(ageTwo)
+
+	nameThree := "Dila Azzura"
+	ageThree := 13
+	fmt.Println(nameThree)
+	fmt.Println(ageThree)
+}
